3c640366fa99e0db968d899bf8fcc2cde522cbae
Jango <rafaeljangodev@gmail.com>
2025-01-01 19:29:28 -0300

    ✨ feat: Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
+
 import { Container } from "./styles";
+import { Header } from "@components/Header";
 
 export function Groups() {
   return (
     <Container>
-    
+      <Header />
     </Container>
   );
 }

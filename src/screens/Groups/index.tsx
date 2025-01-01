ee714d73efef351accc8b0d0d8d91a2bd642970f
Jango <rafaeljangodev@gmail.com>
2025-01-01 20:47:58 -0300

    ✨ feat: GroupCard, Highlight e ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} activeOpacity={0.9} />
+        )}
+        contentContainerStyle={groups.length == 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
